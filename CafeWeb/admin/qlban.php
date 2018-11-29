fbca2573216b2b26a0bb3805da9dd10c5da7b7e8
guendinchoin <trongnguyen158.tn@gmail.com>
2018-11-29 22:39:23 +0700

    29/11

---
+++ CafeWeb/admin/qlban.php
@@ -0,0 +1,25 @@
+<div class="col-lg-6 col-md-6 col-sm-12-col-xs-12 table-bordered">
+	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+		<a href="#" class="text-center">
+			<h3 class="glyphicon glyphicon-glass"></h3><br/>Bàn 1
+		</a>
+	</div>
+	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+		<h3 class="glyphicon glyphicon-glass"></h3><br/>Bàn 2
+	</div>
+	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+		<h3 class="glyphicon glyphicon-glass"></h3><br/>Bàn 3
+	</div>
+	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+		<h3 class="glyphicon glyphicon-glass"></h3><br/>Bàn 4
+	</div>
+	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+		<h3 class="glyphicon glyphicon-glass"></h3><br/>Bàn 5
+	</div>
+	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+		<h3 class="glyphicon glyphicon-glass"></h3><br/>Bàn 6
+	</div>
+</div>
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+	Do something when select a table!!!
+</div>
