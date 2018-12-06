baae7a27451f9668e611b3cf999c7521e12d8ee7
Hoang <pqhoang.17it2@sict.udn.vn>
2018-12-06 17:46:59 +0700

    test
    test

--- CafeWeb/admin/js/qlban.js
+++ CafeWeb/admin/js/qlban.js
@@ -1,11 +1,49 @@
-var checkstart = false;
 // button vao`
 function startup(id) {
-
-
   $("div#ordertable").addClass("hidden");
   $("div#detailtable").removeClass("hidden");
+  $("div#exchange").removeClass("hidden");
   $("#ban").html(id);
+  var ban = "ban" + id;
+  var myck = "";
+  if ($.cookie("ghep" + ban) != undefined) {
+    myck = $.cookie("ghep" + ban);
+  } else {
+    myck = " ";
+  }
+
+
+
+  //đổ giữ liệu từ cookie ra bảng
+  $.ajax({
+    type: 'post',
+    url: "ajaxcall.php",
+    data: {
+      tableid: ban
+
+    },
+    success: function(response) {
+      if (response == "empty") {
+        $('#joindate').html("");
+        $('#ordertab').html('<thead><tr><th scope="col">ST</th><th scope="col">Tên hàng</th><th scope="col">Giá</th><th scope="col">Số lượng</th><th scope="col">Thành tiền</th></tr></thead><tbody id="databody"></tbody>');
+        $('table tbody tr td .task input:checkbox').prop('checked', false);
+        $('#start').prop('disabled', false);
+        $('#totalbill').html(0);
+        $('#timesum').html("");
+        $('#note').html("");
+      } else {
+        $('table tbody tr td .task input:checkbox').prop('checked', false);
+        var obj = JSON.parse(response);
+        $("#joindate").html(obj.datajoin);
+        $("#databody").html(obj.table);
+        $('#totalbill').html(obj.bill);
+        $('#note').html(myck);
+        alert(response);
+
+      }
+
+    }
+  });
 
 
 
@@ -15,41 +53,91 @@ function startup(id) {
 function startorder() {
   var date = new Date().toLocaleString();
   $("#joindate").html(date);
-  checkstart = true;
+  $('#start').prop('disabled', true);
 }
 
 // Button ket thuc
 function endup() {
-  var date = new Date().toLocaleString();
-  $("#leftdate").html(date);
+  var enddate = new Date().toLocaleString();
+  var startdate = $("#joindate").html();
+
+  $("#leftdate").html(enddate);
+  // startdate =	startdate.split(',')[1];
+  // startdate = startdate.replace("PM", " ").trim();
+  startdate = datetoseconds(startdate);
+  enddate = datetoseconds(enddate);
+
+  $("#timesum").html(((enddate - startdate) / 60).toFixed(0) + " Phút");
+}
+
+// convert date to seconds
+function datetoseconds(startdate) {
+  startdate = startdate.split(',')[1];
+  startdate = startdate.replace("PM", " ").trim();
+  startdate = startdate.replace("AM", " ").trim();
+  var a = startdate.split(':'); // split it at the colons
+  // minutes are worth 60 seconds. Hours are worth 60 minutes.
+  var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+  return seconds;
 }
 
 // button huy ban
 function removedt() {
-  alert("asdasd");
-  $('#joindate').html(" ");
-  $('#databody').html(" ");
-  $('table tbody tr td .task input:checkbox').removeAttr('checked');
+  var banc = $('#ban').html();
+  var tb = "ban" + banc
+
+  $.ajax({
+    type: 'post',
+    url: "ajaxcall.php",
+    data: {
+      destroyban: tb
+
+
+    },
+    success: function(response) {
+
+      $('#joindate').html(" ");
+      $('#leftdate').html(" ");
+      $('#databody').html(" ");
+      $('#totalbill').html(0);
+      $('table tbody tr td .task input:checkbox').prop('checked', false);
+      $('#' + banc).css("color", "#0060B6");
+      alert(response);
+    }
+  });
 
 }
 
 //  check box them data table
 $(document).ready(function() {
-  var tbid = '';
-  $(':checkbox').on('change', function() {
-    var $this = $(this);
 
+  var tbid = '';
+
+  $(':checkbox').on('change', function() {
+    var bill = 0;
+    var $this = $(this);
+    var str = $this.val();
+    var pos = $this.val().indexOf("|");
+    var prname = str.substr(0, pos);
+    var prprice = str.substr(pos + 1, str.length);
     if (this.checked) {
-      var str = $this.val();
-      var pos = $this.val().indexOf("|");
-      var prname = str.substr(0, pos);
-      var prprice = str.substr(pos + 1, str.length);
+
       $('#databody')
-        .append('<tr id="tr_' + $this.data('ref') + '" tabindex="0" data-cb="' + $this.data('ref') + '" class="itemadd iteamqty" style="cursor:pointer;"><th >' + $this.data('ref') + '</th><td class="prname">' + prname + '</td><td >' + prprice + '</td><td class="pr-qty">1</td><td class="prprice">' + prprice + '</td></tr>');
+        .append('<tr id="tr_' + $this.data('ref') + '" tabindex="0" data-cb="' + $this.data('ref') + '" class="itemadd iteamqty" style="cursor:pointer;"><td >' + $this.data('ref') + '</td><td class="prname">' + prname + '</td><td >' + prprice + '</td><td class="pr-qty">1</td><td class="prprice">' + prprice + '</td></tr>');
+      $('#ordertab tbody tr').each(function() {
+
+        bill += +$('td', this).eq(4).text(); //+ will convert string to number
+      });
+      $('#totalbill').html(bill);
+
 
     } else {
-      removeCheckedResult($('.itemadd[data-cb=' + $this.data('ref') + ']'));
 
+      removeCheckedResult($('.itemadd[data-cb=' + $this.data('ref') + ']'));
+      $('#ordertab tbody tr').each(function() {
+        bill += +$('td', this).eq(4).text(); //+ will convert string to number
+      });
+      $('#totalbill').html(bill);
     }
 
     // var totalChecked = $('input[type=checkbox][class=mainlist]:checked').length >= 2;
@@ -62,8 +150,12 @@ $(document).ready(function() {
     var name = $(this)
       .find(".prname")
       .text();
+    var qty2 = $(this).find(".pr-qty").text();
+
 
     $('#editqty').val(name);
+    $('#qtyvalue').val(qty2);
+
     tbid = $(this).attr('id');
 
   });
@@ -71,6 +163,28 @@ $(document).ready(function() {
   $(document).on('click', '#huymon', function() {
     $("#" + tbid).remove();
     $('#editqty').val(" 	");
+    var ban = $('#ban').html();
+    var joindate = $('#joindate').html();
+    var tabledata = $('#databody').html();
+    var bill = 0;
+    $('#ordertab tbody tr').each(function() {
+
+      bill += +$('td', this).eq(4).text(); //+ will convert string to number
+    });
+    $('#totalbill').html(bill);
+    $.ajax({
+      type: 'post',
+      url: "ajaxcall.php",
+      data: {
+        datatable: tabledata,
+        ban: ban,
+        joindate: joindate
+
+      },
+      success: function(response) {
+
+      }
+    });
   });
 
   // Tang giam so luong /// flus or sub caculator
@@ -95,13 +209,42 @@ $(document).ready(function() {
 
     // update quantity table data
 
-    $("#" + tbid + " td").eq(2).html(newVal);
+    $("#" + tbid + " td").eq(3).html(newVal);
 
     // update price data
-    var prqty = $("#" + tbid + " td").eq(1).html();
+    var prqty = $("#" + tbid + " td").eq(2).html();
     prqty *= newVal;
     total += prqty;
-    $("#" + tbid + " td").eq(3).html(total);
+    $("#" + tbid + " td").eq(4).html(total);
+
+    var bill = 0;
+    $('#ordertab tbody tr').each(function() {
+      bill += +$('td', this).eq(4).text(); //+ will convert string to number
+    });
+    $('#totalbill').html(bill);
+
+
+    var ban = $('#ban').html();
+    var joindate = $('#joindate').html();
+    var tabledata = $('#databody').html();
+    $.ajax({
+      type: 'post',
+      url: "ajaxcall.php",
+      data: {
+        datatable: tabledata,
+        ban: ban,
+        joindate: joindate
+
+      },
+      success: function(response) {
+
+      }
+    });
+
+
+
+
+
 
   });
 
@@ -114,3 +257,110 @@ $(document).ready(function() {
 
   }
 });
+
+
+
+//random color
+$(document).ready(function() {
+  var colors = ['#03dac6', '#f48fb1', '#ce93d8', '#d1c4e9', '#bbdefb', '#cddc39', '#ffb74d', '#eeeeee', '#b0b3c5'];
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  $('.menu-right').css('background-color', random_color);
+});
+
+
+
+
+
+$(document).ready(function() {
+  $(document).on('click', '#setcookie', function() {
+    var ban = $('#ban').html();
+    var joindate = $('#joindate').html();
+    var tabledata = $('#databody').html();
+    var money = $('#totalbill').html();
+    $("div#ordertable").removeClass("hidden");
+    $("div#detailtable").addClass("hidden");
+    $("div#exchange").addClass("hidden");
+
+
+    // var array = [];
+    // var headers = [];
+    // $('#ordertab thead tr th').each(function(index, item) {
+    //     headers[index] = $(item).html();
+    // });
+    // $('#ordertab tbody tr').has('td').each(function() {
+    //     var arrayItem = {};
+    //     $('td', $(this)).each(function(index, item) {
+    //         arrayItem[headers[index]] = $(item).html();
+    //     });
+    //     array.push(arrayItem);
+    // });
+
+    $.ajax({
+      type: 'post',
+      url: "ajaxcall.php",
+      data: {
+        datatable: tabledata,
+        ban: ban,
+        joindate: joindate,
+        bill: money
+
+      },
+      success: function(response) {
+        if (response == "empty") {
+          $('#joindate').html("");
+          $('#totalbill').html("");
+          $('#ordertab').html('<thead><tr><th scope="col">ST</th><th scope="col">Tên hàng</th><th scope="col">Giá</th><th scope="col">Số lượng</th><th scope="col">Thành tiền</th></tr></thead><tbody id="databody"></tbody>');
+
+        } else {
+          $("#" + ban).css("color", "#2ecc71");
+        }
+      }
+    });
+
+
+
+
+  });
+  $(document).on('click', '#chuyenban', function() {
+    $('.modal-header #titlemd').html("Chuyển bàn");
+
+  });
+  $(document).on('click', '#ghepban', function() {
+    $('.modal-header #titlemd').html("Ghép bàn");
+
+  });
+  $(document).on('click', '#tachban', function() {
+    $('.modal-header #titlemd').html("Tách bàn");
+
+  });
+  $(document).on('click', '#confirm', function() {
+    var banc = $('#ban').html();
+    var action = $('.modal-header #titlemd').html();
+    var newbat = $('#optional option:selected').val();
+    var oldbat = "ban" + banc;
+
+
+
+    $.ajax({
+      type: 'post',
+      url: "ajaxcall.php",
+      data: {
+        action: action,
+        newbat: newbat,
+        oldbat: oldbat
+      },
+      success: function(response) {
+        if (response == "empty") {
+          console.log("lỗi" + response);
+
+        } else {
+          alert(response);
+          location.reload();
+        }
+      }
+
+    });
+
+  });
+});
+//resources.php
